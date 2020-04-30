7295bd85c94282a7adda04894d953de258d86ce2
Josee9988 <jgracia9988@gmail.com>
2020-04-30 17:30:31 +0200

    "added basic initializartion and pipe"

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { FilterPipe } from './filter.pipe';
+
+
+@NgModule({
+  declarations: [
+  FilterPipe],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
